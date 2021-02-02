561b69a19c878b2e2cea8c9f8bec7c428ff188bc
pulpo1993 <marcelo_1993@hotmail.es>
2021-02-02 10:35:01 -0500

    inicio prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ export class AppComponent {
   public appPages = [
     { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' }, 
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
